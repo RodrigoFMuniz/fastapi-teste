74a426bff96a8863f1134632e3271fb62f52742f
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-12 22:59:27 -0300

    Beerlog app

--- beerlog/models.py
+++ beerlog/models.py
@@ -1,8 +1,10 @@
-from dataclasses import dataclass
+from matplotlib.pyplot import table
+from sqlmodel import SQLModel, Field
+from sqlmodel import select
 
 
-@dataclass
-class Beer:
+class Beer(SQLModel, table=True):
+    id: int = Field(primary_key=True)
     name: str
     style: str
     flavor: int
@@ -11,3 +13,14 @@ class Beer:
 
 
 brewdog = Beer(name='Brewdog', style='NEIPA', flavor=6, image=8, cost=8)
+
+
+# from dataclasses import dataclass
+
+# @dataclass
+# class Beer:
+#     name: str
+#     style: str
+#     flavor: int
+#     image: int
+#     cost: int
