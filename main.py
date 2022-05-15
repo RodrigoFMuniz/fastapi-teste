fca44401adf30cd13c5b6c40c22d2931cc5fde07
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-14 23:46:42 -0300

    Showing behavior when trying to apply a new non dynamic route after dynamic routes

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ def new_route():
 def about():
     return 'About page'
 
+@app.get('/blog/unpublished')
+def images():
+    return {f'data':'all unpublished'}
+
 @app.get('/rota/{id}')
 def param_route(id):
     return {'Id':id}
@@ -28,3 +32,8 @@ def comments(id):
 @app.get('/blog/{id}/content')
 def contents(id: int):
     return {f'content {id}':id}
+    
+@app.get('/blog/{id}/images')
+def images(id: int):
+    return {f'images {id}':id}
+
