9dc1476d0547bf11785076fae0d3e5fcc6b70ad7
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-16 23:24:07 -0300

    Adding database, declaretive base

--- sql_app/db.py
+++ sql_app/db.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
                        "check_same_thread": False})
+
+base = declarative_base()
