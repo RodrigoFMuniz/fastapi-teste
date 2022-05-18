66bc8d4f35ab40cf62c2622b1cc43ec34138441c
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-05-17 23:52:57 -0300

    Main.py update

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from sql_app.schemas import Blog
+from sql_app import models, schemas
+from sql_app.db import engine
 
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
 
 @app.post('/blog')
 def create(request: Blog):
